c6a0312e1ed5670d4313ec8c113544d64a10d67e
amirahmadadibi <amirahmadadibi@gmail.com>
2021-10-19 21:48:56 +0330

    add start point main function

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
 //the first thing that we need is import material design
 import 'package:flutter/material.dart';
 
+//add starting point of the app
+void main() {
+  return runApp(BullsEyeApp());
+}
+
 //create base class for app
 class BullsEyeApp extends StatelessWidget {
   @override
