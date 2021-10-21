c82f84fdde61bc89bce4d42be9fb1881a5149d66
amirahmadadibi <amirahmadadibi@gmail.com>
2021-10-21 12:58:06 +0330

    disable nullbility checks

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,18 @@
+//remove two start and this line would disable nullbility for ths dart file
+
+//**@dart=2.9
+
+// or
+
+/*
+
+  or 
+  environment:
+  sdk: ">=2.11.0 <3.0.0"
+  at pubspec.yaml file
+
+*/
+
 //the first thing that we need is import material design
 import 'package:flutter/material.dart';
 
@@ -15,4 +30,9 @@ class BullsEyeApp extends StatelessWidget {
         theme: ThemeData(primarySwatch: Colors.blue),
         home: GamePage(title: 'BullsEye'));
   }
+}
+
+class GamePage extends StatefulWidget {
+  GamePage({Key key, this.title}) : super(key: key);
+  var title;
 }
